4c945cd7f1168120d41328e5f9974b011b63ee72
Yusheng Su <yushengsu.thu@gmail.com>
2024-05-01 02:37:58 +0400

    work on the training loss

--- code/distill_llm.py
+++ code/distill_llm.py
@@ -109,7 +109,7 @@ class LargerModel:
         x = x.to(self.model.device)
         z = self.model(x, output_hidden_states=True)
         return z
-    
+
 
 class SmallerModel:
     def __init__(self, parser):
@@ -134,8 +134,8 @@ class SmallerModel:
         #     cache_dir = self.cache_dir,
         #     trust_remote_code=True
         # )
-       
-        
+
+
         self.model_config = AutoConfig.from_pretrained(self.llm, trust_remote_code=True)
         # self.model_config.rotary_dim = int(self.model_config.rotary_dim / self.reduction_factor)
         self.model_config.n_embd = int(self.model_config.n_embd / self.reduction_factor)
@@ -148,26 +148,26 @@ class SmallerModel:
             self.model_config,
             trust_remote_code=True
         )
-        
+
         # for name, param in self.model.named_parameters():
         #    print(name, param.shape)
 
         # exit()
-        
-        
+
+
         # self.model = AutoModelForCausalLM.from_pretrained(
         #     self.llm,
         #     revision = self.revision,
         #     cache_dir = self.cache_dir,
         #     trust_remote_code=True
-        # ) 
+        # )
 
-        # downsampling the weights 
+        # downsampling the weights
         self.reduce()
 
         #for name, param in self.model.named_parameters():
         #    print(name, param.shape)
-    
+
     def reduce(self):
         # # Create a copy of the state_dict for modifications
 
@@ -176,10 +176,10 @@ class SmallerModel:
             revision = self.revision,
             cache_dir = self.cache_dir,
             trust_remote_code=True
-        ) 
-        
+        )
+
         state_dict = self.model.state_dict()
-        
+
         # Iterate over the state_dict and modify parameters
         # for name, param in model_original.named_parameters():
         #count=0
@@ -189,8 +189,8 @@ class SmallerModel:
                 # 2D weight matrices
                 if param.size(0) == param.size(1):
                     # Subsample and scale square matrices
-                    #param.data = self._subsample_and_scale(param_original, param) 
-                    state_dict[name] = self._subsample_and_scale(param_original, param) 
+                    #param.data = self._subsample_and_scale(param_original, param)
+                    state_dict[name] = self._subsample_and_scale(param_original, param)
                 else:
                     # Handle rectangular matrices by subsampling only the larger dimension
                     if "wte" in name:
@@ -206,16 +206,16 @@ class SmallerModel:
 
         #del model_original
         # Load the modified state_dict back to the model
-        self.model.load_state_dict(state_dict, strict=True) 
+        self.model.load_state_dict(state_dict, strict=True)
 
-        
+
     # 1 D
     def _subsample_embeddings(self, matrix_original, matrix_target):
         #print(embeddings.shape)
         indices = torch.arange(0, matrix_original.size(0), self.reduction_factor)
         out_dim = int(indices.shape[0])
         target_d0 = int(matrix_target.shape[0])
-        if out_dim == target_d0: 
+        if out_dim == target_d0:
             pass
         else:
             indices = indices[:target_d0]
@@ -227,8 +227,8 @@ class SmallerModel:
         indices = torch.arange(0, matrix_original.size(0), self.reduction_factor)
         #subsampled_matrix = matrix_original[indices][:, indices] * self.reduction_factor
         out_dim = int(indices.shape[0])
-        target_d0, target_d1 = int(matrix_target.shape[0]), int(matrix_target.shape[1]) 
-        if out_dim == target_d0: 
+        target_d0, target_d1 = int(matrix_target.shape[0]), int(matrix_target.shape[1])
+        if out_dim == target_d0:
             pass
         else:
             indices = indices[:target_d0]
@@ -236,8 +236,8 @@ class SmallerModel:
             pass
         else:
             indices = indices[:target_d1]
-        subsampled_matrix = matrix_original[indices, :][:, indices] * self.reduction_factor 
-        return subsampled_matrix 
+        subsampled_matrix = matrix_original[indices, :][:, indices] * self.reduction_factor
+        return subsampled_matrix
 
     def _subsample_embeddings_dim(self, matrix_original, matrix_target):
         # Determine which dimension is larger
@@ -247,39 +247,39 @@ class SmallerModel:
         indices_1 = torch.arange(0, matrix_original.size(1), self.reduction_factor)
         out_dim_0 = int(indices_0.shape[0])
         out_dim_1 = int(indices_1.shape[0])
-        target_d0, target_d1 = int(matrix_target.shape[0]), int(matrix_target.shape[1]) 
-        if out_dim_0 == target_d0: 
+        target_d0, target_d1 = int(matrix_target.shape[0]), int(matrix_target.shape[1])
+        if out_dim_0 == target_d0:
             pass
         else:
             indices_0 = indices_0[:target_d0]
         if out_dim_1 == target_d1:
             pass
         else:
-            indices_1 = indices_1[:target_d1]        
+            indices_1 = indices_1[:target_d1]
         subsampled_matrix = matrix_original[indices_0, :][: ,indices_1]
-        return subsampled_matrix 
-        
+        return subsampled_matrix
+
     def _subsample_embeddings_dim0(self, matrix_original, matrix_target):
         indices = torch.arange(0, matrix_original.size(0), self.reduction_factor)
-        out_dim_0 = int(indices.shape[0]) 
-        target_d0 = int(matrix_target.shape[0]) 
-        if out_dim_0 == target_d0: 
+        out_dim_0 = int(indices.shape[0])
+        target_d0 = int(matrix_target.shape[0])
+        if out_dim_0 == target_d0:
             pass
         else:
             indices = indices[:target_d0]
         subsampled_matrix = matrix_original[indices, :]
-        return subsampled_matrix 
+        return subsampled_matrix
 
     def _subsample_embeddings_dim1(self, matrix_original, matrix_target):
         indices = torch.arange(0, matrix_original.size(1), self.reduction_factor)
-        out_dim_1 = int(indices.shape[0]) 
-        target_d1 = int(matrix_target.shape[1]) 
-        if out_dim_1 == target_d1: 
+        out_dim_1 = int(indices.shape[0])
+        target_d1 = int(matrix_target.shape[1])
+        if out_dim_1 == target_d1:
             pass
         else:
             indices = indices[:target_d1]
         subsampled_matrix = matrix_original[:, indices]
-        return subsampled_matrix 
+        return subsampled_matrix
 
 
 
@@ -289,11 +289,11 @@ class SmallerModel:
         #     # Subsample only along the larger dimension
         #     indices = torch.arange(0, matrix.size(0), self.reduction_factor)
         #     subsampled_matrix = matrix[indices, :]
-        #     return subsampled_matrix 
+        #     return subsampled_matrix
         # else:
         #     indices = torch.arange(0, matrix.size(1), self.reduction_factor)
         #     subsampled_matrix = matrix[:, indices]
-        #     return subsampled_matrix 
+        #     return subsampled_matrix
 
 
     def forward(self, x):
@@ -343,7 +343,7 @@ class Distiller:
 
         self.show_params(self.larger_model.model)
         self.show_params(self.smaller_model.model)
-        
+
 
         '''
         self.opt = bnb.optim.Lion(
@@ -356,11 +356,11 @@ class Distiller:
         )
         '''
         self.opt = optim.AdamW(self.smaller_model.model.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
-        
+
         #self.distill_model = torch.compile(distill_model)
         #optimizer = optim.AdamW(self.smaller_model.parameters(), lr=self.learning_rate, weight_decay=parser.weight_decay)
         # Add seduchler
-        
+
         ###
         '''
         - device_placement (default: True): Automatically place the model and data on the right device (CPU or GPU).
@@ -378,9 +378,9 @@ class Distiller:
         - gradient_accumulation_steps (default: 1): Number of steps to accumulate gradients before updating model parameters, which can be useful for effectively increasing the batch size without increasing the memory consumption.
         '''
         ###
-        
+
         #fsdp_plugin = FSDPPlugin(mixed_precision="bf16", reshard_after_forward=True)
-         
+
         self.accelerator = Accelerator(
             gradient_accumulation_steps=self.grad,
             #mixed_precision = 'fp8',
@@ -389,7 +389,7 @@ class Distiller:
             #megatron_lm_plugin = True,
             #deepspeed_plugin = True,
         )
-        
+
         self.larger_model, self.smaller_model, self.opt, self.loader= self.accelerator.prepare(
             self.larger_model, self.smaller_model, self.opt, self.loader
         )
@@ -452,7 +452,7 @@ class Distiller:
     def compute_mean_loss(self, large_hidden_states, small_hidden_states):
         # print(large_hidden_states.shape) #torch.Size([layers of the model, batch_size, seq_length, output_dim])
         # print(small_hidden_states.shape) #torch.Size([layers of the model, batch_size, seq_length, output_dim])
-        mean_large = large_hidden_states.mean(dim=(1, 2, 3)) 
+        mean_large = large_hidden_states.mean(dim=(1, 2, 3))
         mean_small = small_hidden_states.mean(dim=(1, 2, 3))
         mean_loss = torch.abs(mean_large - mean_small).sum()
         return mean_loss
@@ -474,7 +474,7 @@ class Distiller:
         dist_large = Normal(mean_large, std_large)
         dist_small = Normal(mean_small, std_small)
         # caculate KL by layer
-        #kl_div = kl_divergence(dist_large, dist_small).mean(dim=1)  
+        #kl_div = kl_divergence(dist_large, dist_small).mean(dim=1)
         kl_div = kl_divergence(dist_large, dist_small).mean(dim=(1, 2, 3)).sum()
         return kl_div
 
@@ -489,7 +489,7 @@ class Distiller:
 
         large_hidden_states = output_large.hidden_states
         large_hidden_states = torch.stack(large_hidden_states)
-        small_hidden_states = output_small.hidden_states  
+        small_hidden_states = output_small.hidden_states
         small_hidden_states = torch.stack(small_hidden_states)
         # print("======")
         # print(large_hidden_states.shape) #torch.Size([33, 4, 2048, 4096])
@@ -499,15 +499,15 @@ class Distiller:
         # print("======")
 
 
-        # compute_mean_loss of large_hidden_states and small_hidden_states for each layer 
+        # compute_mean_loss of large_hidden_states and small_hidden_states for each layer
         # calculate the overall loss as the sum of mean_loss and wasserstein_distance
         mean_loss = self.compute_mean_loss(large_hidden_states, small_hidden_states)
-        # compute_kl_distance of large_hidden_states and small_hidden_states for each layer 
+        # compute_kl_distance of large_hidden_states and small_hidden_states for each layer
         kl_loss = self.compute_kl_divergence_distance(large_hidden_states, small_hidden_states)
-        print("ml:", mean_loss.shape)
-        print("kl:", kl_loss.shape)
+        #print("ml:", mean_loss.shape)
+        #print("kl:", kl_loss.shape)
         loss = mean_loss + kl_loss
-        
+
         return loss
 
 
@@ -541,22 +541,22 @@ class Distiller:
         max_i = 0
         stop_batch = self.train_max_batch
         accumulated_loss = 0.0
-        
+
         self.larger_model.model.to(self.device)
         self.smaller_model.model.to(self.device)
-        
-        # Need to revise 
+
+        # Need to revise
         #half: fp16
-        self.larger_model.model.half() 
-        self.smaller_model.model.half() 
-         
+        self.larger_model.model.half()
+        self.smaller_model.model.half()
+
         self.larger_model.model.train()
         self.smaller_model.model.train()
-        
+
         print()
         print("lr:{}".format(self.learning_rate))
         for i, batch in enumerate(prog):
-            
+
             self.opt.zero_grad()
 
             if i == stop_batch:
@@ -566,7 +566,7 @@ class Distiller:
 
             output_large = self.larger_model.forward(batch)
             output_small = self.smaller_model.forward(batch)
-            loss = self.loss_layer(output_large, output_small) 
+            loss = self.loss_layer(output_large, output_small)
             accumulated_loss += loss.item()
             total_loss += loss.item()
             prog.set_description(f"loss: {loss.item():.3f}")
@@ -580,20 +580,20 @@ class Distiller:
             '''
 
             self.accelerator.backward(loss)
-            
-            ''' 
-            #################### 
+
+            '''
+            ####################
             # check whether optimize the parmaeters?
             for initial_param, trained_param in zip(self.larger_model.model.parameters(), self.smaller_model.model.parameters()):
                 if not torch.equal(initial_param, trained_param):
                     print("Parameter changed!")
                 else:
                     print("Parameter unchanged.")
-            #################### 
-            ''' 
-            
-            self.accelerator.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
-             
+            ####################
+            '''
+
+            #self.accelerator.clip_grad_norm_(self.smaller_model.model.parameters(), max_norm=1.0)
+
             self.opt.step()
             continue
 
@@ -601,7 +601,7 @@ class Distiller:
                 self.opt.step()
                 prog.set_description(f"accumulated_loss: {total_loss:.3f}")
                 accumulated_loss = 0.0
-                
+
         total_loss = total_loss/self.step
         print()
         print(f"total_loss: {total_loss:.3f}")
@@ -626,7 +626,7 @@ def main():
     parser.add_argument('--reduction_factor', type=int, default=4, help='reduction_factor')
 
     parser.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
+
 
     args = parser.parse_args()
     #args.checkpoint = os.getcwd()+"/../checkpoint/" + args.checkpoint
@@ -636,6 +636,6 @@ def main():
     distiller = Distiller(args, larger_model, smaller_model)
     distiller.distill()
 
-    
-if __name__ == "__main__":  
+
+if __name__ == "__main__":
     main()
